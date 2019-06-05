2e8685a177eaac0188975e5ca3ea481a5143a6c6
Jessie <jessiempaxton@gmail.com>
2019-06-05 15:53:53 -0600

    push
    Co-authored-by: Jason Walker <evansjudah@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,47 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="nav">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <router-link class="navbar-brand" to="/">MJ</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
+        aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/about">GitHub<span class="sr-only">(current)</span></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search">
+          <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+  export default {
+    name: 'Navbar',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  }
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  a {
+    color: #42b983;
+  }
 </style>
